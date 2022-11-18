bcfe8ad279b91755ce898904c06064bc8d78c513
viktor <moiseev0506@mail.ru>
2022-11-18 19:11:45 +0300

    version 1

---
+++ Person/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>
+            {
+                new User("Viktor",16,177),
+                new User("Raziyat",17,175),
+                new User("Magomed",17,184),
+                new User("Abdulhakim",20,186),
+                new User("Ruslan",17,172),
+
+            };
+            var first = users.Where(item => item.age>18).ToList();
+            int n = 0;
+            foreach(var item in first)
+            {
+
+                n++;
+            }
+            Console.WriteLine($"Пользователей старше 18-ти лет: {n}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Person/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+     public class User
+    {
+
+        public User(string name, int age, double height)
+        {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+
+        public string name { get; set; }
+        public int age { get; set; }
+        public double height { get; set; }
+    }
+}
